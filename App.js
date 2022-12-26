38bf4bb949a02c73c8a863b79eb33dd4e154fb43
shalithacode <shalithalakruwan1998@gmail.com>
2022-12-27 00:47:55 +0530

    Parsing requests

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><label for="name">Product name:</label><br><input type="text" id="name" name="title" ><br><input type="submit" value="Add Product"></form>'
+  );
 });
-app.use((req, res, next) => {
-  res.send("<h1>Hello</h2>");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
-app.use((req, res, next) => {});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home</h2>");
+});
 
 app.listen(3000);
