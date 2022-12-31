8c15e3e797416d54ecfbcc112fcb8aa8a94d4957
shalithacode <shalithalakruwan1998@gmail.com>
2022-12-31 19:35:55 +0530

    Add new navigation

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formCSS: true,
@@ -18,7 +18,7 @@ exports.postAddprouduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       pageTitle: "Shop",
       prods: products,
       path: "/",
