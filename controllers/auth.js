c6c90310ab0776977779a609f528ecb7249d2f07
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-09-17 02:14:34 +0530

    Added signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+const bcrypt = require("bcryptjs");
 exports.getLogin = (req, res, next) => {
   if (!req.session.isLoggedIn) {
     res.render("auth/login", {
@@ -28,4 +28,37 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect("/login");
   });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAutheticated: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) return res.redirect("/signup");
+
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          user.save();
+        })
+        .then((result) => res.redirect("/login"));
+    })
+
+    .catch((err) => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const authController = require("../controllers/auth");
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 module.exports = router;
